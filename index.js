3b5afc768272747875507338e390ba2301bd6d04
NIckChagun <sikoo2544@gmail.com>
2024-01-06 17:40:43 +0700

    Update INterview

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ let port = process.env.PORT || 3000;
 
 app.set('view engine','ejs')
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => { 
     res.render('home');
 });
